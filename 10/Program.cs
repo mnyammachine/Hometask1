fb4d3ffd9a7c3107ddac57a8295dd9f1b4420e6f
Ksenia Marchenko <106731538+mnyammachine@users.noreply.github.com>
2023-02-03 08:58:07 +0200

    Update Program.cs

--- 10/Program.cs
+++ 10/Program.cs
@@ -3,4 +3,3 @@ int n = new Random().Next (100, 1000);
 Console.WriteLine ("Number = " + n);
 int digit = n / 10 % 10;
 Console.WriteLine("Искомое число = " + digit);// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
