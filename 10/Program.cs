470368069872598528e548a0e927ff8434232cbb
Ksenia <mnyam@mail.ru>
2023-02-03 08:12:06 +0200

    закоммитила программку

---
+++ 10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа принимает трехзначное число, на выходе показывает вторую цифру этого числа 
+int n = new Random().Next (100, 1000);
+Console.WriteLine ("Number = " + n);
+int digit = n / 10 % 10;
+Console.WriteLine("Искомое число = " + digit);// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
